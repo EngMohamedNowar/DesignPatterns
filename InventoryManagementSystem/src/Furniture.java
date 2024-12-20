ab647ad51891045f49fe8ad53e32404ee137c5a7
Habiba Elbehairy <habiba.elbehairy1@gmail.com>
2024-12-20 23:08:34 +0200

    Delete InventoryManagementSystem directory

--- InventoryManagementSystem/src/Furniture.java
+++
@@ -1,5 +0,0 @@
-// Furniture.java: Concrete Product for Factory Pattern
-class Furniture extends Product {
-    public Furniture() { this.name = "Furniture"; }
-    public void create() { System.out.println("Creating Furniture Product"); }
-}
